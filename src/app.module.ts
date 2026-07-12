2c21744c83b437a6f95f067124c9fd9f0e812135
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-12 17:52:27 -0300

    Creado archivos en blanco de jugador

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JugadorModule } from './jugador/jugador.module';
 
 @Module({
-  imports: [],
+  imports: [JugadorModule],
   controllers: [AppController],
   providers: [AppService],
 })
