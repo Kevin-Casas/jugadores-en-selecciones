a2a0dab1cf388984292394c60b520efa8dd6645a
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-12 22:05:18 -0300

    Agregados metodos Get esqueletos

--- src/jugador/jugador.controller.ts
+++ src/jugador/jugador.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
 @Controller('jugador')
-export class JugadorController {}
+export class JugadorController {
+  //Retorna todos los jugadores en la base de datos
+  @Get()
+  getTodosLosJugadores() {}
+
+  //Retorna todos los jugadores de cierto pais
+  @Get(':pais')
+  getJugadoresDePais(@Query('pais') pais: string) {}
+
+  //Retorna un jugador con un id determinado
+  @Get(':id')
+  getJugador(@Query('id') id: Number) {}
+}
