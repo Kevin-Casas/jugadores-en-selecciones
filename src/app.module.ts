19e22787b9c3a0ab633615565ecd65bfc0fa19d7
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-12 22:32:52 -0300

    Instalado e importado nestjs config para uso de variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { JugadorModule } from './jugador/jugador.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [JugadorModule],
+  imports: [
+    JugadorModule,
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
