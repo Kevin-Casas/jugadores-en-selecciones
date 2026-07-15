6dca14bb223ab34e24ec117a63ab01fac021508e
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-15 19:27:22 -0300

    Metodos basicos en jugadores.controller

--- src/jugador/jugador.controller.ts
+++ src/jugador/jugador.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Put, Delete } from '@nestjs/common';
 import { JugadorService } from './jugador.service';
+import { CreateJugadorDto } from './dto/create-jugador-dto';
+import { UpdateJugadorDto } from './dto/update-jugador-dto';
 
 @Controller('jugador')
 export class JugadorController {
   constructor(private readonly jugadorService: JugadorService) {}
   //Retorna todos los jugadores en la base de datos
   @Get()
-  getTodosLosJugadores() {}
+  getTodosLosJugadores() {
+    return this.jugadorService.findAll();
+  }
 
   //Retorna todos los jugadores de cierto pais
   @Get(':pais')
@@ -18,5 +22,25 @@ export class JugadorController {
 
   //Retorna un jugador con un id determinado
   @Get(':id')
-  getJugador(@Query('id') id: Number) {}
+  getJugador(@Query('id') id: number) {
+    return this.jugadorService.findOne(id);
+  }
+
+  //Agregar un jugador
+  @Post(':id')
+  crearJugador(@Body() dto: CreateJugadorDto) {
+    return this.jugadorService.create(dto);
+  }
+
+  //Actualiza los datos de un jugador
+  @Put(':id')
+  actualizarJugador(@Param('id') id: number, @Body() dto: UpdateJugadorDto) {
+    return this.jugadorService.update(id, dto);
+  }
+
+  //Elimina un jugador
+  @Delete(':id')
+  borrarJugador(@Param('id') id: number) {
+    return this.jugadorService.remove(id);
+  }
 }
