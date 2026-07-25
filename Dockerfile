b4823ccc951ea42c819ed1174d0a886df2c5e27d
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-25 15:45:46 -0300

    Arreglo de dependencias y Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -16,9 +16,6 @@ FROM node:24-slim
 
 WORKDIR /app
 
-COPY package*.json ./
-RUN npm ci --omit=dev
-
 COPY --from=builder /app/package*.json ./
 COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/dist ./dist
--- src/jugador/dto/update-jugador-dto.ts
+++ src/jugador/dto/update-jugador-dto.ts
@@ -1,4 +1,4 @@
-import {PartialType} from '@Nestjs/mapped-types';
+import {PartialType} from '@nestjs/mapped-types';
 import { CreateJugadorDto } from './create-jugador-dto';
 
 export class UpdateJugadorDto extends PartialType(CreateJugadorDto) {}
