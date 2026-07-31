a0c36dd69ab571f6684c6fddd8b6069f0faf1b7a
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-07-31 00:32:42 -0300

    Modificacion de middleware para que muestre el codigo de respuesta de las peticiones

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,11 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
-  console.log({
-    method: req.method,
-    url: req.url,
-    ip: req.ip,
-    timeStamp: new Date().toISOString(),
+  const start = Date.now();
+
+  res.on('finish', () => {
+    console.log({
+      method: req.method,
+      url: req.originalUrl,
+      ip: req.ip,
+      timeStamp: new Date().toISOString(),
+      statusCode: res.statusCode,
+      duration: `${Date.now() - start}ms`,
+    });
   });
+
   next();
 }
