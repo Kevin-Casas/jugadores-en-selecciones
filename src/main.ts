a894d697b8aac014c57e19061d23296a93d3a5b4
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-08-01 23:16:00 -0300

    Cambio de lugar de carpeta de interceptores

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { TransformInterceptor } from './jugador/interceptors/transform.interceptor';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
