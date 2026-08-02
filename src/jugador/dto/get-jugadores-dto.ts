4fa84fc765d5d65c00d631ab21a6e9183efd3118
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-08-02 12:17:02 -0300

    Implementado Swagger a Query en enpoint Get

--- src/jugador/dto/get-jugadores-dto.ts
+++ src/jugador/dto/get-jugadores-dto.ts
@@ -1,12 +1,15 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsIn, IsOptional, Matches } from 'class-validator';
 
 export class BuscarJugadoresDto {
+  @ApiPropertyOptional()
   @IsOptional()
   @Matches(/^[A-Za-zÀ-ÿ\s]+$/, {
     message: 'pais no puede contener letras o simbolos',
   })
   pais?: string;
 
+   @ApiPropertyOptional()
   @IsOptional()
   @IsIn(['Delantero', 'Arquero', 'Defensor', 'Mediocampista'])
   posicion?: string;
