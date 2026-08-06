7e0d304caafcf7ba51106ebedd248226aff448e9
Kevin-Casas <89878241+Kevin-Casas@users.noreply.github.com>
2026-08-06 18:24:27 -0300

    agregar validacion para valores numericos positivos

--- src/jugador/dto/create-jugador-dto.ts
+++ src/jugador/dto/create-jugador-dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
 
 export class CreateJugadorDto {
   @ApiProperty({ example: 'Lionel Messi', description: 'Nombre del Jugador' })
@@ -15,34 +15,34 @@ export class CreateJugadorDto {
   posicion!: string;
 
   @ApiProperty({ example: '5', description: 'Goles hechos por el jugador' })
-  @IsNumber()
+  @IsPositive()
   goles!: number;
 
   @ApiProperty({
     example: '10',
     description: 'Asistencias hechas por el jugador',
   })
-  @IsNumber()
+  @IsPositive()
   asistencias!: number;
 
   @ApiProperty({
     example: '1',
     description: 'Tarjetas rojas otorgadas jugador',
   })
-  @IsNumber()
+  @IsPositive()
   tarjetas_rojas!: number;
 
   @ApiProperty({
     example: '1',
     description: 'Tarjetas amarillas otorgadas jugador',
   })
-  @IsNumber()
+  @IsPositive()
   tarjetas_amarillas!: number;
 
   @ApiProperty({
     example: '500',
     description: 'Partidos en los que el jugador participo',
   })
-  @IsNumber()
+  @IsPositive()
   partidos_jugados!: number;
 }
